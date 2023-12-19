625d961ff626947ce8c068ca57e71726dbcea771
Martin Sandberg <martisan@kth.se>
2023-12-19 11:21:38 +0100

    Leaderboard and game over in one
    Slight delay when rendering gameOver

--- src/presenters/LeaderBoardPresenter.jsx
+++ src/presenters/LeaderBoardPresenter.jsx
@@ -3,19 +3,33 @@ import GameOverView from "../views/GameOverView.jsx";
 
 export default function LeaderBoardPresenter(props) {
 
-
+/*
+<GameOverView 
+            winner={props.model.winner}
+            sessionID={props.model.sessionID}
+            leaderboard={props.model.leaderboard}
+            routeDestination={`/home/${props.model.user.uid}`} />
+*/
 
     if (props.model.gameOver) {
         // If the game is over, render the GameOverView
-        return <GameOverView 
+        return <LeaderBoardView
+            gameOver={props.model.gameOver}
             winner={props.model.winner}
+            title={"Game Over"}
+            btnLabel={"Exit"}
             sessionID={props.model.sessionID}
             leaderboard={props.model.leaderboard}
             routeDestination={`/home/${props.model.user.uid}`} />
         } else {
         return <LeaderBoardView
+            gameOver={props.model.gameOver}
+            winner={props.model.winner}
+            title={""}
+            btnLabel={"Continue"}
             sessionID={props.model.sessionID}
             leaderboard={props.model.leaderboard}
             routeDestination={`/game/${props.model.sessionID}/${props.model.user.uid}`} />
+
         }
 }
